a0f9697bd36266ea1f2f7c00d15615f97bfc32af
Nicolas Rousseau <71373738+NeikoStream@users.noreply.github.com>
2023-12-07 22:54:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
-php
-extends("header.php");
+<?php include 'header.php';?>
+
+<!-- Contenu de votre page -->
+<main>
+  <h2>Contenu de ma page</h2>
+  <p>Contenu...</p>
+</main>
+
+<?php include 'footer.php';?>
