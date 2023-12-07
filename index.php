aaa9bc08fdefd986c05e103e7f3be8d8b2abc4e7
PuissantsyOficial <153216594+PuissantsyOficial@users.noreply.github.com>
2023-12-07 23:57:27 +0100

    Ninth version

--- index.php
+++ index.php
@@ -3,6 +3,21 @@
 <!-- Contenu de votre page -->
 <main>
 
+<div id="carte_choix">
+  <div class="cartes" id="carte1">
+    <h2>Usine Nucléaire</h2>
+    <img src="image/usine_nucléaire.png" alt="image">
+    <p>Cette Usine a pour objectif de remplacer les usines à charbon pour une énergie plus verte mais cette énergie pollue aussi la planète </p>
+
+  </div>
+  <div class="cartes" id="carte1">
+    <h2>Usine Nucléaire</h2>
+    <img src="image/usine_nucléaire.png" alt="image">
+    <p>Cette Usine a pour objectif de remplacer les usines à charbon pour une énergie plus verte mais cette énergie pollue aussi la planète </p>
+    
+  </div>
+</div>
+
 <div id="indicateur" class="card w-2">
     <h1 id="indicateur_titre">Indicateur</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, dolorem. Labore, illum.</p>
@@ -10,8 +25,12 @@
 
 <div id="cadrage_image">
 <img id="taille_planète" src="./image/icon.png" alt="La planète">
-<a href="carte.php">test</a>
+
 </div>
+
 </main>
 
 <?php include 'footer.php';?>
+
+
+
