b10d4f365b9d92319ba5a7e8f87210d0d616b675
Nicolas Rousseau <71373738+NeikoStream@users.noreply.github.com>
2023-12-08 03:35:20 +0100

    header

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
-<footer class="footer mt-auto py-3 bg-light">
-    <div class="container">
-        <span class="text-muted">© Equipe Pas de Nom</span>
-    </div>
-</footer>
+<footer class="footer mt-auto py-3 bg-light fixed-bottom">
+        <div class="container">
+            <span class="text-muted">© 2023 Votre Nom. Tous droits réservés.</span>
+        </div>
+    </footer>
