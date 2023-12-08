86ca1bd3d587cb86378b95a5b7dc44fb8bf14d03
PuissantsyOficial <153216594+PuissantsyOficial@users.noreply.github.com>
2023-12-08 01:00:36 +0100

    Tenth version

--- index.php
+++ index.php
@@ -3,22 +3,7 @@
 <!-- Contenu de votre page -->
 <main>
 
-<div id="carte_choix">
-  <div class="cartes" id="carte1">
-    <h2>Usine Nucléaire</h2>
-    <img src="image/usine_nucléaire.png" alt="image">
-    <p>Cette Usine a pour objectif de remplacer les usines à charbon pour une énergie plus verte mais cette énergie pollue aussi la planète </p>
-
-  </div>
-  <div class="cartes" id="carte1">
-    <h2>Usine Nucléaire</h2>
-    <img src="image/usine_nucléaire.png" alt="image">
-    <p>Cette Usine a pour objectif de remplacer les usines à charbon pour une énergie plus verte mais cette énergie pollue aussi la planète </p>
-    
-  </div>
-</div>
-
-<div id="indicateur" class="card w-2">
+<div id="indicateur" class="card">
     <h1 id="indicateur_titre">Indicateur</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt, dolorem. Labore, illum.</p>
 </div>
@@ -27,7 +12,24 @@
 <img id="taille_planète" src="./image/icon.png" alt="La planète">
 
 </div>
-
+<div id="carte_choix">
+  <div class="cartes" id="carte1">
+    <h2>Usine Nucléaire</h2>
+    <img src="image/usine_nucléaire.png" alt="image">
+    <p>Cette Usine a pour objectif de remplacer les usines à charbon pour une énergie plus verte mais cette énergie pollue aussi la planète </p>
+    <div>
+      <p>+100CO2/s</p>
+    </div>
+  </div>
+  <div class="cartes cartes_identité" id="carte2">
+    <h2>Usine Nucléaire</h2>
+    <img src="image/usine_nucléaire.png" alt="image">
+    <p>Cette Usine a pour objectif de remplacer les usines à charbon pour une énergie plus verte mais cette énergie pollue aussi la planète </p>
+    <div>
+      <p>+100CO2/s</p>
+    </div>
+  </div>
+</div>
 </main>
 
 <?php include 'footer.php';?>
